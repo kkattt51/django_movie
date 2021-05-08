a5f5cad91f3b0600c2fcaff71cd457ab9baadcde
kkattt51 <10tany2010.10@gmail.com>
2021-05-08 16:11:29 +0300

    Initial commit

---
+++ movies/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Rewiews
+
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Rewiews)
+
